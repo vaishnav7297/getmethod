ca72860ec91dc76387d738ced046b29941be8fe1
vaishnav7297 <vaishnav7297@gmail.com>
2020-06-22 07:52:35 +0530

    get method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { Comments } from './classes/comments';
 import { Component } from '@angular/core';
+import { freeApiService } from './services/freeapi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'get-test';
+  constructor(private _freeApiService: freeApiService) {
+    
+  }
+  lstcomments:Comments[];
+  
+  ngOnInit() {
+    this._freeApiService.getcomments()
+     .subscribe
+    (
+      data=>
+     {
+     
+       this.lstcomments = data;
+
+    }
+    );
+  }
 }
