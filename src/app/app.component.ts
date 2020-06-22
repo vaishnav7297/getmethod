01648412aa33ec6fc8e2f403af712c78aa55aecf
vaishnav7297 <vaishnav7297@gmail.com>
2020-06-22 20:48:15 +0530

    City list updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Comments } from './classes/comments';
 import { Component } from '@angular/core';
 import { freeApiService } from './services/freeapi.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +10,46 @@ import { freeApiService } from './services/freeapi.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private _freeApiService: freeApiService) {
-    
-  }
-  lstcomments:Comments[];
-  
-  ngOnInit() {
-    this._freeApiService.getcomments()
-     .subscribe
-    (
-      data=>
-     {
-     
-       this.lstcomments = data;
+  url = environment.api_url;
+  cities = [];
+  constructor(
+    private _freeApiService: freeApiService,
+    private http: HttpClient
+  ) {}
+  lstcomments: Comments[];
 
-    }
-    );
+  ngOnInit() {
+    this.gettoken();
+    this._freeApiService.getcomments().subscribe((data) => {
+      this.lstcomments = data;
+    });
+  }
+  gettoken() {
+    const headers = new HttpHeaders({
+      Accept: 'application/json',
+      'api-token':
+        '1cMA3yNYCvJ3PL2FW86o-gtrIFHnZbkFZyJ3Q6X7uwoUvWvbuabro4G0nKTXfLzxWpo',
+      'user-email': 'sreerajkarippala@gmail.com',
+    });
+
+    this.http
+      .get(environment.api_url + 'api/getaccesstoken', { headers: headers })
+      .subscribe((Response: any) => {
+        console.log(Response.auth_token);
+        this.getcities(Response.auth_token);
+      });
+  }
+  getcities(token: string) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + token,
+      Accept: 'application/json',
+    });
+
+    this.http
+      .get(environment.api_url + 'api/countries/', { headers: headers })
+      .subscribe((Response: any) => {
+        console.log(Response);
+        this.cities = Response;
+      });
   }
 }
--- src/app/services/freeapi.service.ts
+++ src/app/services/freeapi.service.ts
@@ -4,8 +4,10 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class freeApiService {
-  constructor(private httpclient: HttpClient){}
+  constructor(private httpclient: HttpClient) {}
   getcomments(): Observable<any> {
-    return this.httpclient.get("https://jsonplaceholder.typicode.com/posts/1/comments");
+    return this.httpclient.get(
+      'https://jsonplaceholder.typicode.com/posts/1/comments'
+    );
   }
 }
